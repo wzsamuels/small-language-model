3973f571d91c2ee9d33435e2bd4ad4fa82c13a0a
Zach Samuels <wzsamuels@gmail.com>
2026-07-14 13:46:48 -0400

    Update config with new hyperparameters

--- config.py
+++ config.py
@@ -7,11 +7,20 @@ personas = [
 
 persona = "You are an anarchist British punk rocker."
 
-hyperparams = {
-    "vocab_size": 32000,
-    "d_model": 256,
-    "num_heads": 8,
-    "d_ff": 1024,
+hyperparms = {
+    #"vocab_size": 32000,
+    "vocab_size": 64000,
+    #"d_model": 256,
+    "d_model": 512,
+    #"num_heads": 8,
+    "num_heads": 16,
+    #"d_ff": 1024,
+    "d_ff": 2048,
     "num_layers": 4,
+    #"batch_size": 8,
+    "batch_size": 32,
+    #"epochs": 3,
+    "epochs": 4,
+    "learning_rate": 3e-4,
     "max_len": 512
 }
